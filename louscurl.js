cbc7c2856185d099b563746bae88a148a1bf40ff
abcjcce <seawee65@live.com>
2020-05-16 13:42:20 -0400

    installed actual Promises

--- louscurl.js
+++ louscurl.js
@@ -1,75 +1,81 @@
 /// note: uses the following from schedule.js
 /// const days = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa']
 
-function promiseSchedule(classCodes, pfunc)
+function promiseSchedule(classCodes)
 {
-	if (!classCodes) {
-		pfunc('')
-		return
-	}
-	
-	const schedule = []
-	const goal = classCodes.length
-	let curr = 0
-	
-	if (goal == 0) { pfunc(''); return }
-	
-	for (let classCode of classCodes) {
-		promiseClass(classCode, classes => {
-			schedule.push(...classes)
-			if (++curr == goal)
-				pfunc(schedule.filter(c => c != null))
-		})
-	}
+	return new Promise(resolve => {
+		if (!classCodes) {
+			resolve('')
+			return
+		}
+
+		const schedule = []
+		const goal = classCodes.length
+		let curr = 0
+
+		if (goal == 0) { resolve(''); return }
+
+		for (let classCode of classCodes) {
+			promiseClass(classCode).then(classes => {
+				schedule.push(...classes)
+				if (++curr == goal)
+					resolve(schedule.filter(c => c != null))
+			})
+		}
+	})
 }
 
 function promiseClass(classCode, pfunc)
 {
-	scurl("https://louslist.org/sectiontip.php?ClassNumber=" + classCode, text => {
-		const title = text.match(/(class="InfoClass">)(.*)(<br)/)[2]
-		const tals = Array.from(text.matchAll(/(?<=\/td><td>)(.*?)(?:<\/td><td>)(.*?)(?=<\/td><\/tr>)/g)).map(groups => {
-			
-			let timedow  = groups[1].toLowerCase()
-			let location = groups[2]
-			
-			let times = timedow.match(/\d+:\d+(?:am|pm)/g)
-			
-			if (!times)
-				return null
-			
-			return {
-				title: title,
-				loc:   location,
-				dows:  timedow.match(/mo|tu|we|th|fr|sa|su/g).map(name => days.indexOf(name)),
-				start: toHRT(times[0]),
-				end:   toHRT(times[1])
-			}
-			
+	return new Promise(resolve =>
+		scurl("https://louslist.org/sectiontip.php?ClassNumber=" + classCode).then(text => {
+			const title = text.match(/(class="InfoClass">)(.*)(<br)/)[2]
+			const tals = Array.from(text.matchAll(/(?<=\/td><td>)(.*?)(?:<\/td><td>)(.*?)(?=<\/td><\/tr>)/g)).map(groups => {
+
+				let timedow  = groups[1].toLowerCase()
+				let location = groups[2]
+
+				let times = timedow.match(/\d+:\d+(?:am|pm)/g)
+
+				if (!times)
+					return null
+
+				return {
+					title: title,
+					loc:   location,
+					dows:  timedow.match(/mo|tu|we|th|fr|sa|su/g).map(name => days.indexOf(name)),
+					start: toHRT(times[0]),
+					end:   toHRT(times[1])
+				}
+
+			})
+
+			resolve(tals)
 		})
-		
-		pfunc(tals)
-	})
+	)
 }
 
 /* takes a 12-hour time string
  * returns a float that represents the time in hours (referred to as an HRT)
- * 
+ *
  * "8:30PM" -> 20.5
 */
 function toHRT(text)
 {
 	const nums = text.match(/\d+/g).map(n => Number(n))
 	const ampm = text.toLowerCase().match(/am|pm/g)[0] === 'pm' ? 12 : 0
-	
+
 	return (ampm + nums[0] % 12) + nums[1] / 60
 }
 
 /* takes a url and a function
  * runs the function with the url's response text
 */
-function scurl(url, cfunc)
+function scurl(url)
 {
-	fetch('https://simplecorsworkaround.herokuapp.com/?url=' + encodeURIComponent(url))
-		.then(resp => resp.text())
-		.then(text => cfunc(text))
+	return new Promise(resolve =>
+		fetch('https://simplecorsworkaround.herokuapp.com/?url=' + encodeURIComponent(url))
+			.then(resp => resp.text())
+			.then(text => resolve(text))
+	)
 }
